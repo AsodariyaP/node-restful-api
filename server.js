27bbc1e6e2a9474a15eb6588ff878f814a7daa0f
Pradip Asodariya <asodariyapradip@gmail.com>
2017-11-19 18:05:20 +0530

    Add server.js
    Create  server with Node/Express

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/userModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Userdb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/userRoutes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+console.log('user RESTful API server started on: ' + port);
