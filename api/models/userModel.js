90eeea661f5d41b5cfa1b14f9bf382a47bb475f9
Pradip Asodariya <asodariyapradip@gmail.com>
2017-11-19 18:07:19 +0530

    Add Models.js
    Create model file for validations and  data-types model defines here.

---
+++ api/models/userModel.js
@@ -0,0 +1,36 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Please enter name'
+    },
+    surname: {
+        type: String,
+        required: 'Please enter surname'
+    },
+    age: {
+        type: Number,
+        required: 'Please enter age'
+    },
+    city: {
+        type: String,
+        required: 'Please enter city'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    gender: {
+        type: [{
+            type: String,
+            enum: ['male', 'female', 'other']
+        }],
+        default: ['male']
+    }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
